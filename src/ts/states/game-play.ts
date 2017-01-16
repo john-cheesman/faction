a68d34d5e35cf678e53ae270ca242e4879e8028c
John <John>
2017-01-16 08:22:03 +0000

    Add missing GamePlay class

---
+++ src/ts/states/game-play.ts
@@ -0,0 +1,16 @@
+export default class GamePlay extends Phaser.State {
+    public gamePlayData: any;
+
+    public map: Phaser.Tilemap;
+
+    public layers: Object;
+
+    init(gamePlayData: any) {
+        this.gamePlayData = gamePlayData;
+
+        this.map = this.game.add.tilemap(gamePlayData.map.key);
+
+        this.map.addTilesetImage(gamePlayData.map.tilesetImage.name, gamePlayData.map.tilesetImage.key);
+    }
+
+}
